f8f7da2cc5a60b5cf84eccab62a328a60cc453c2
wingedseraph <r89647@gmail.com>
2025-11-24 18:50:45 +0300

    chore(not-found): update component to use optional error prop and enhance link styling

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,14 +1,17 @@
-import type { ReactNode } from "react";
 import { Link } from "react-router-dom";
 
 import { Path } from "@/config/routes-config";
 
-export default function NotFound({ error }: { error: string }): ReactNode {
+type NotFoundProps = {
+  error?: string;
+};
+
+export default function NotFound({ error }: NotFoundProps) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-2">
       <h1 className="font-bold">page not found</h1>
       <p>error: {error}</p>
-      <Link className="transition-all" to={Path.index}>
+      <Link className="transition-all duration-300 ease-in-out hover:opacity-80" to={Path.index}>
         ← to index page
       </Link>
     </div>
