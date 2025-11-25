091b215958a6932c71b02d6b092614b783604a24
wingedseraph <r89647@gmail.com>
2025-11-25 18:23:59 +0300

    chore(deploy): remove unused files from dist directory

--- dist/assets/styles-BJZqDuDb.js
+++
@@ -1,6 +0,0 @@
-import{c as d,j as a,L as l}from"./index-CPMDuNsm.js";import{c as u}from"./index-BYYEpi5c.js";/**
- * @license lucide-react v0.554.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const f=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],m=u("arrow-left",f);function p(i){const o=d.c(4),{label:n,to:s}=i,e=n===void 0?"Back to list":n,c=s===void 0?"/products":s;let r;o[0]===Symbol.for("react.memo_cache_sentinel")?(r=a.jsx(m,{className:"mr-1 h-5 w-5"}),o[0]=r):r=o[0];let t;return o[1]!==e||o[2]!==c?(t=a.jsxs(l,{className:"mb-8 inline-flex items-center text-sm text-muted-foreground transition-colors hover:text-card-foreground",to:c,children:[r,e]}),o[1]=e,o[2]=c,o[3]=t):t=o[3],t}const g="rounded-3xl border border-border bg-card shadow-sm",h="w-full rounded-3xl border bg-card px-3 py-2 text-card-foreground transition-all outline-none",k="focus:border-accent focus:ring-1 focus:ring-accent",v="border-destructive focus:ring-1 focus:ring-destructive",w="border-border focus:border-accent focus:ring-1 focus:ring-accent";export{p as B,v as a,w as b,k as c,g as d,h as i};
